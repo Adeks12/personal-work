174caf8736a5ef189c48f8603806eeb4f1d09d5a
unknown <Jesuslovestestimony@gmail.com>
2025-03-05 00:24:44 +0100

    first push

---
+++ users.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@example.com",
+  },
+
+  {
+    id: 2,
+    name: "Thomas",
+    email: "Thomas@example.com",
+  },
+
+  {
+    id: 3,
+    name: "Gospel",
+    email: "Gospel@example.com",
+  },
+
+  {
+    id: 4,
+    name: "Anthony",
+    email: "Anthony@example.com",
+  },
+];
+module.exports = users;
